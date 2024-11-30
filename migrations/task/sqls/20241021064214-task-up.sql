8767d87b67aad70d71fe9645c32fa7bd0742f416
tzuhuangyen <912yan@gmail.com>
2024-11-30 16:37:43 +0100

    UPDATE 1-4

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -32,12 +32,6 @@ where email ='opcatiy0@hexschooltest.io';
 select count(*) from "USER"
 -- 1-5 查詢：取得 USER 資料表所有用戶資料，並列出前 3 筆（提示：使用limit語法）
 
-SELECT
-  *
-FROM
-  "USER"
-LIMIT
-  3;
 --  ████████  █████   █    ████  
 --    █ █   ██    █  █         █ 
 --    █ █████ ███ ███       ███  
