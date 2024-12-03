36f791deb05e0084cafd39b177cd98bcc6eefba6
tzuhuangyen <912yan@gmail.com>
2024-12-03 21:43:07 +0100

    update3-4

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -140,7 +140,7 @@ and u.name in ('肌肉棒子','Q太郎');
 
 -- 3-4 刪除：新增一個專長 空中瑜伽 至 SKILL 資料表，之後刪除此專長。
 insert into "SKILL" (name)
-values('空中瑜伽')
+values('空中瑜伽');
 
 DELETE FROM "SKILL"
 WHERE name = '空中瑜伽';
